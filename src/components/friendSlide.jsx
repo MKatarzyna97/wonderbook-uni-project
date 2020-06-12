60ae59084bde55d18858ecbabe718bb886e5b09b
MKatarzyna97 <malicka.kkatarzyna@gmail.com>
2020-06-12 21:36:30 +0200

    friend section done

--- src/components/friendSlide.jsx
+++ src/components/friendSlide.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import Friends from '../photos/friends.jpg'
 
 class FriendSlide extends Component {
     state = {  }
     render() { 
         return ( <div className="friend-slide">
-            <h2>Przyjaźń</h2>
+            <img src={Friends} alt=""/>
+           <div className="friend-slide-elements"> 
+           <h1>Temat miesiąca</h1>
+           <h2>Przyjaźń</h2>
             <div className="kochanowski">
             <p> <i>Co bez przyjaciół za żywot? Więzienie, <br/> W którym niesmaczne żadne dobre mienie.</i> </p>
-            <p>Jan Kochanowski</p>
+            <h4>Jan Kochanowski</h4>
             </div>
-            <p> Co miesiąc wybieramy hasło, które jest motywem przewodnim wybranych dla Ciebie książek. <br/>
 
-                Celebruj przyjaźń i zobacz nasze propozycje: </p> </div> );
+            <div className="friend-desciption">
+            <p> Co miesiąc wybieramy hasło, które jest motywem przewodnim wybranych dla Ciebie książek. <br/>
+            W tym miesiącu tematem przewodnim jest bliska naturze człowieka <b>przyjaźń</b>. <br/>
+
+                Celebruj przyjaźń i zobacz nasze propozycje: </p> 
+                
+                <button className="read-more-friend">Dowiedz się więcej</button> </div> </div> </div> );
     }
 }
  
