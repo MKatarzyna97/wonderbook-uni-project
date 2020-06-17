aed4badcdd3f8eddd0b33f8bd6bbac212a39bc03
MKatarzyna97 <malicka.kkatarzyna@gmail.com>
2020-06-17 02:58:53 +0200

    friend section style changed

--- src/components/friendSlide.jsx
+++ src/components/friendSlide.jsx
@@ -6,7 +6,7 @@ class FriendSlide extends Component {
     state = {  }
     render() { 
         return ( <div className="friend-slide">
-            <img src={Friends} alt=""/>
+           
            <div className="friend-slide-elements"> 
            <h1>Temat miesiąca</h1>
            <h2>Przyjaźń</h2>
@@ -17,11 +17,12 @@ class FriendSlide extends Component {
 
             <div className="friend-desciption">
             <p> Co miesiąc wybieramy hasło, które jest motywem przewodnim wybranych dla Ciebie książek. <br/>
-            W tym miesiącu tematem przewodnim jest bliska naturze człowieka <b>przyjaźń</b>. <br/>
-
+            W tym miesiącu tematem przewodnim jest bliska naturze człowieka <b>przyjaźń</b>. 
+<br/>
                 Celebruj przyjaźń i zobacz nasze propozycje: </p> 
                 
-             <Link to="this-month"> <button className="read-more-friend">Dowiedz się więcej</button> </Link> </div> </div> </div> );
+             <Link to="this-month"> <button className="read-more-friend">Dowiedz się więcej</button> </Link> </div> </div>
+             <img src={Friends} alt=""/> </div> );
     }
 }
  
