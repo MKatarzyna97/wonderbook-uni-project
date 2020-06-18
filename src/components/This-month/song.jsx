00f51935dfed0906676b120743d3e6feb8bf1d70
MKatarzyna97 <malicka.kkatarzyna@gmail.com>
2020-06-18 11:54:31 +0200

    styling

--- src/components/This-month/song.jsx
+++ src/components/This-month/song.jsx
@@ -1,8 +1,56 @@
 import React, { Component } from 'react';
 
 class Song extends Component {
-    state = {  }
+    state = { 
+        showLyrics: false
+     }
+
+     LyricsOn = (event) => {
+        event.preventDefault();
+        this.setState({
+          showLyrics: true
+        })
+      }
+
+      LyricsOff = (event) => {
+        event.preventDefault();
+        this.setState({
+          showLyrics: false
+        })
+      }
     render() { 
+
+        let fullLyrics = (<p>
+            Ja w tobie
+Ty we mnie <br/>
+Zaklęci <br/>
+Wzajemnie <br/>
+Bez granic <br/>
+I miary <br/>
+Dobrani <br/>
+Do pary(...) <br/> 
+<br/>
+W lustrze cię widzę przed sobą co dzień <br/>
+Za tobą chodzi z kolei mój cień <br/>
+Jedna istota i taki sam plan <br/>
+Komunikuję sam sobie twój stan <br/>
+Ty jesteś księgą tak samo jak ja <br/>
+Będę cię czytał wieczorem co dnia <br/>
+Poprzez granice przeniosę twą treść <br/>
+Zawsze cię będę ze sobą już nieść <br/>
+<br/>
+Ja w tobie <br/>
+Ty we mnie <br/>
+Zaklęci <br/>
+Wzajemnie <br/>
+Bez granic <br/>
+I miary <br/>
+Dobrani <br/>
+Do pary(...) <br/>
+        </p>)
+
+
+
         return ( <div className='song'>
         <div className='song-lyrics'>
             <h3>Grzegorz Turnau</h3>
@@ -15,8 +63,12 @@ class Song extends Component {
         Będę cię czytał wieczorem co dnia <br/>
         Poprzez granice przeniosę twą treść <br/>
         Zawsze cię będę ze sobą już nieść</p>
+        {this.state.showLyrics ? fullLyrics : null}
+        {this.state.showLyrics ? <button onClick={this.LyricsOff}>Off</button> : <button onClick={this.LyricsOn}>On</button> }
+       
+
         </div>
-        <iframe className="video" width="560" height="315" src="https://www.youtube.com/embed/6eZePDp0hAM" 
+        <iframe className="video" title="Dobrani do pary" width="560" height="315" src="https://www.youtube.com/embed/6eZePDp0hAM" 
         frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
         allowfullscreen></iframe>
         </div> );
